adb42b4def29a53defed37b051ef92613d9e340c
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2024-06-26 19:50:32 +0900

    merge into feature/hyoan-payment

--- src/action/cartActions.js
+++ src/action/cartActions.js
@@ -32,6 +32,7 @@ const getCartList = () => async (dispatch) => {
     dispatch({ type: types.GET_CART_LIST_REQUEST });
     const response = await api.get('/cart');
     if (response.status !== 200) throw new Error(response.error);
+    console.log(response, 'responsesssssss');
     dispatch({ type: types.GET_CART_LIST_SUCCESS, payload: response.data.data });
   } catch (error) {
     dispatch({ type: types.GET_CART_LIST_FAIL, payload: error.message });
